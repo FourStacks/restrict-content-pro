742a9929ee6418f882edc8d0b34a59e51be8ef78
Ashley Evans <ashley@nosegraze.com>
2017-05-04 13:08:47 +0100

    Move pending verification check to RCP_Member::can_access() #782

--- includes/class-rcp-member.php
+++ includes/class-rcp-member.php
@@ -1179,6 +1179,11 @@ class RCP_Member extends WP_User {
 		 * From this point on we assume the post has some kind of restrictions added.
 		 */
 
+		// If the user is pending email verification, they don't get access.
+		if ( $this->is_pending_verification() ) {
+			return apply_filters( 'rcp_member_can_access', false, $this->ID, $post_id, $this );
+		}
+
 		// If the user doesn't have an active account, they don't get access.
 		if( $this->is_expired() || ! in_array( $this->get_status(), array( 'active', 'free', 'cancelled' ) ) ) {
 			return apply_filters( 'rcp_member_can_access', false, $this->ID, $post_id, $this );
